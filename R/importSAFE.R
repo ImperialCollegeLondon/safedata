ccb69757fcf4eaae241828cf630ab2c6f4156fa6
ajaldersley <a.aldersley@imperial.ac.uk>
2019-04-16 18:20:54 +0100

    added simple function to process summary info and placeholders for other processing functions

--- R/importSAFE.R
+++ R/importSAFE.R
@@ -14,6 +14,44 @@ readTransposedXlsx <- function (file, sheetName) {
   dfT <- as.data.frame(t(df[-1]), stringsAsFactors=FALSE)
   names(dfT) <- t(df[,1])
   dfT <- as.data.frame(lapply(dfT, type.convert))
+  
   return(dfT)
 }
 
+processSummary <- function (df) {
+  #' Process summary "meta" information for SAFE project data
+  #' 
+  #' Reads and assigns variables from the provided dataframe, which must be a
+  #' transposed "Summary" sheet from a SAFE project data submission.
+  #' 
+  #' @param dataframe Dataframe containing summary information
+  #' @return List object containing summary information
+  
+  safeObj <- list()
+  safeObj$projectID <- df$SAFE.Project.ID[1]
+  safeObj$title <- as.character(df$Title[1])
+  safeObj$startDate <- as.Date.numeric(df$Start.Date[1], origin="1899-12-30")
+  safeObj$endDate <- as.Date.numeric(df$End.Date[1], origin="1899-12-30")
+  
+  return(safeObj)
+}
+
+processTaxa <- function (dataframe, safeObj) {
+  #' Adds taxa information to SAFE data object
+}
+
+processLocations <- function (dataframe, safeObj) {
+  #' Adds location information to SAFE data object
+}
+
+processData <- function (file, safeObj) {
+  #' Adds data worksheets to SAFE data object
+}
+
+safeWrapper <- function (file) {
+  #' Wrapper to open and process SAFE data file
+}
+
+printSummary <- function (safeObj) {
+  #' print summary information for SAFE data object
+}
