88dd191f3226578b6e8daea12e1e7606c0f52c17
dorme <d.orme@imperial.ac.uk>
2019-06-27 12:18:39 +0100

    Removed roxygen styled comment from .onLoad - don't want it to appear in documentations

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function (libname, pkgname) {
-  #' Function to run on import of package \code{safer}
 
   packageStartupMessage('SAFE package reminder: Please set SAFE_data_dir using setSafeDir()')
+
 }
