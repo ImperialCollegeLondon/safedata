de8ec64633cac02d31afd86ab28aeec1f31bad9e
dorme <d.orme@imperial.ac.uk>
2019-06-27 11:22:06 +0100

    Removed old DO code snippet

--- R/getRecords.R
+++
@@ -1,7 +0,0 @@
-getRecords <-
-function(){
-
-records <- jsonlite::fromJSON(url("http://127.0.0.1:8000/datasets/view_datasets?_export_type=json"))
-records$record_ids <- as.numeric(sapply(records$zenodo_concept_record, basename))
-return(records)
-}
