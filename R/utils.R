c9bd4eeb6eb4723a6e2e245d935d2975a4f7f5e4
dorme <d.orme@imperial.ac.uk>
2019-07-05 11:01:24 +0100

    Much reworking of indexing mechanism: updates on set_safe_dir and adds current availability

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,9 @@
-# Create an environment within the package namespace that is used to 
-# keep a copy of the index, rather than needing to repeatedly read
-# from file.
+#' Dataset index cache
+#'
+#' The \code{safedata.env} environment within the package namespace is 
+#' used to store a copy of the dataset index, rather than needing to 
+#' repeatedly read from file.
+#' @keywords internal
 
 safedata.env <- new.env(parent = emptyenv())
 
@@ -24,87 +27,6 @@ simpleCap <- function (str) {
            collapse = ' '))
 }
 
-setSafeDir <- function(dir, init=FALSE) {
-    #' Set the local SAFE data directory
-    #'
-    #' Validate and sets the local SAFE data directory in options('safedata.dir').
-	#' The named directory must contain the index file ('safe_data_index.rds'),
-	#' although this will be created if \code{init} is set to true.
-	#'
-	#' This function requires access to the internet to query the SAFE Project
-	#' website API.
-    #'
-    #' @param dir A path to the directory to set as the SAFE data directory.
-	#' @param init A boolean to indicate whether to initialise the directory
-	#'     as a SAFE data directory.
-	#' @export
-
-	if (! dir.exists(dir)) {
-        stop("Directory not found.")
-    } 
-	
-	index_found <- file.exists(file.path(dir, 'safe_data_index.rds'))
-	
-	if(init & index_found) {
-		warning("Index already exists, setting as SAFE data directory,.")
-	} else if(init & ! index_found) {
-		message('Downloading SAFE dataset index.')
-		# Get the data frame of files as the index, set local = FALSE for all
-		# and save as an RDS file in the data directory
-		index <- jsonlite::fromJSON('https://www.safeproject.net/api/files')
-		index <- index$entries
-		
-		# add flags to show what information and files are held locally
-		index$local_metadata <- FALSE
-		index$local_file <- FALSE
-		
-		# format the datetime classes
-		index$publication_date <- as.POSIXct(index$publication_date)
-		index$dataset_embargo <- as.POSIXct(index$dataset_embargo)
-		
-		saveRDS(index, file.path(dir, 'safe_data_index.rds'))
-	} else if(! index_found) {
-		stop("Index not found, not a SAFE data directory.")
-	} 
-	
-	# set the directory in options and cache the index 
-	options(safedata.dir = dir)
-	assign('index', readRDS(file.path(dir,'safe_data_index.rds')), safedata.env)
-	
-}
-
-extract_record_id <- function(record_id){
-	#' Extracts a dataset record id from user input
-	#'
-	#' Users could possible submit a range of values as a record id
-	#' and this handler attempts to extract the record id number
-	#' from the possible URLs.
-	
-	if(mode(record_id) == 'numeric'){
-		if(record_id %% 1 == 0){
-			return(record_id)
-		} else {
-			stop('Record id is not an integer')
-		}
-	} else if(mode(record_id) == 'character'){
-		# Could be just a string representation of the integer or
-		# could be a DOI (possibly with URL) or a Zenodo URL
-		# - https://www.zenodo.org/record/3247631#.XRreJ9NKgWo
-		# - https://doi.org/10.5281/zenodo.3247631
-		# - 10.5281/zenodo.3247631
-		# All of which end with the record id, except the ?tracking tag
-		# on Zenodo
-		match <- regexpr('^[0-9]+$|(?<=record/)[0-9]+|(?<=zenodo.)[0-9]+', record_id, perl=TRUE)
-		if(match != -1){
-			record_id <- regmatches(record_id, match)
-			return(as.numeric(record_id))
-		} else {
-			stop('Unknown string record_id representation')
-		}
-	} else {
-		stop('Unknown record_id value')		
-	}
-}
 
 get_data_dir <- function(){
 	#' Get SAFE data directory
@@ -120,32 +42,326 @@ get_data_dir <- function(){
 }
 
 get_index <- function(){
-	#' Get SAFE datasets index
+	
+	#' Get the cached SAFE dataset index
 	#'
 	#' Internal handler to retrieve the local loaded copy of the index from the
 	#' package cache environment
+	#'
 	#' @keywords internal
 	
-	local <- try(get('index', safedata.env), silent=TRUE)
+	index <- try(get('index', safedata.env), silent=TRUE)
 	
 	if(inherits(local, 'try-error')){
 		stop('Failed to load cached index.')
 	}
 	
-	return(local)
+	return(index)
+}
+
+
+get_remote_index <- function(){
+	
+	#' Retrieve current dataset index
+	#' 
+	#' This function downloads the dataset index from the SAFE project
+	#' files API (\url{https://www.safeproject.net/api/files}), formats
+	#' datetime fields and adds local file paths.
+	#'
+	#' @keywords internal
+	
+	# Get the data frame of files as the index, set local = FALSE for all
+	# and save as an RDS file in the data directory
+	
+	index <- jsonlite::fromJSON('https://www.safeproject.net/api/files')
+	index <- index$entries
+		
+	# format the datetime classes
+	index$publication_date <- as.POSIXct(index$publication_date)
+	index$dataset_embargo <- as.POSIXct(index$dataset_embargo)
+	
+	# Add the path relative to the data directory
+	index$path <- with(index, file.path(zenodo_concept_id, zenodo_record_id, filename))
+	
+	return(index)
+	
+}
+
+set_index_availability <- function(index){
+	
+	#' Update dataset availability information
+	#' 
+	#' This function gets the index and sets which datasets are
+	#' currently and which records are the most recent available 
+	#' under a given concept.
+	#'
+	#' @param index An index data frame
+	#' @return An updated index data frame with added fields showing
+	#'   available and most recent available records.
+	#' @keywords internal
+		
+	# identify availability and most recent available records
+	index$available <- with(index, 
+							ifelse(dataset_access == 'embargo' & dataset_embargo >= Sys.time(), FALSE, 
+								   ifelse(dataset_access == 'restricted', FALSE, TRUE)))
+	
+	# get the index rows by concept, reduce to the unique set of records (dropping 
+	# the multiple files), drop unavailable records, sort by publication date and 
+	# return the first zenodo_record_id.
+	concepts <- split(subset(index, select=c(available, zenodo_record_id, publication_date)), 
+					  f=index$zenodo_concept_id)
+
+	mr_avail <- sapply(concepts, function(recs){
+		recs <- unique(recs)
+		recs <- subset(recs, available)
+		recs <- recs[order(recs$publication_date, decreasing=TRUE),]
+		
+		if(nrow(recs)){
+			return(recs$zenodo_record_id[1])
+		} else {
+			return(numeric(0))
+		}
+	})
+	
+	index$most_recent_available <- with(index, ifelse(zenodo_record_id %in% unlist(mr_avail), TRUE, FALSE))
+		 
+	return(index)
+}
+
+
+
+verbose_message <- function(str, ...){
+	
+	#' Provide package messages that can be globally muted
+	#'
+	#' Prints a message if  \code{option('safedata.verbose')}  is set to TRUE.
+	#' Note that individual expressions can be muted using \code{suppressMessages()}
+	#' but this mutes them globally.
+	#' @keywords internal
+	
+	if(options("safedata.verbose")[[1]]){
+		message(str, ...)
+	}
+}
+
+set_safe_dir <- function(safedir, update=TRUE, create=FALSE, validate=TRUE){
+	
+    #' Set the local SAFE data directory
+    #'
+    #' The safedata package maintains a local copy of data files, a dataset 
+	#' index ('safe_data_index.rds') and dataset metadata in this directory.
+	#' The location of the directory is stored in options('safedata.dir') and
+	#' this function is used to point the package to an existing directory, 
+	#' create a new data directory if requested and to maintain the dataset index.
+	#' 
+	#' By default (and by necessity if creating a new data directory), the 
+	#' function requires the internet to update the dataset index, but updating
+	#' can be turned off for offline use.
+    #'
+	#' The default behaviour is also to validate the directory structure. The
+	#' function will warn when files other than those found in datasetes are 
+	#' present within the data structure and when any dataset files that are
+	#' present have been modified. Although this can be turned off, it is 
+	#' not recommended to modify or add files within a SAFE data directory.
+	#' 
+    #' @param safedir A path to the directory to set as the SAFE data directory (str).
+	#' @param update Should the local dataset index be updated (logical)?
+	#' @param create Should a new data directory be created at the provided
+	#'   path (logical)?
+	#' @param validate Should the directory structure be validated (logical)?
+	#' @return NULL
+	#' @export
+
+	
+	index_path <- file.path(safedir,'safe_data_index.rds')
+	
+	# Handle create first
+	if(create) {
+		if(dir.exists(safedir)) {
+			# We don't want a existing directory that might have stuff in it
+			stop('Directory already exists')
+		} 
+		
+		# create the directory
+		dir.create(safedir)
+		
+		# create the new index file, stash in cache, set the options and end
+		index <- get_remote_index()
+		saveRDS(index, index_path)
+		index <- set_index_availability(index)
+		assign('index', index, safedata.env)
+		options(safedata.dir = safedir)
+		verbose_message('Safe data directory created')
+		return(invisible())
+	} 
+	
+	# Now validate an existing directory
+	
+	if (! dir.exists(safedir)) {
+        stop("Directory not found.")
+    } 
+	
+	if (! file.exists(index_path)) {
+		stop("Dataset index not found.")
+	}
+	
+	# At this point we have an existing index file, so update unless told not to
+	index <- readRDS(index_path)
+	
+	if(update){
+		verbose_message('Updating index...')
+		remote <- get_remote_index()
+		
+		# Currently just using the set of zenodo record ids to test.
+		# It is conceivable that entries could change after being created,
+		# so maybe need a more complex reset at some point to look for 
+		# datasets that have changed.
+		if(setequal(index$zenodo_record_id, remote$zenodo_record_id)) {
+			verbose_message('Index up to date')
+		} else if(length(setdiff(index$zenodo_record_id, remote$zenodo_record_id)) > 0){
+			verbose_message('Local index contains retracted or unknown records, contact data@safeproject.net')
+		} else {		
+			# Update the index cache and RDS file
+			saveRDS(remote, file.path(safedir, 'safe_data_index.rds'))
+			index <- remote
+			verbose_message('Index updated')
+		}
+	}
+	
+	if(validate){
+		verbose_message('Validating directory')		
+		# Run a check on directory structure
+		local_files <- dir(safedir, recursive=TRUE)
+		local_files <- local_files[! grepl('.rds$', local_files)]	
+	
+		local_unexpected <- setdiff(local_files, index$path)
+		if(length(local_unexpected)){
+			warning('SAFE data directory contains unexpected files: ', paste(local_unexpected, collapse=', '))
+		}
+	
+		# Run a check on file modification
+		local_expected <- subset(index, file.exists(file.path(safedir, index$path)))
+		local_expected$local_md5 <- tools::md5sum(path.expand(file.path(safedir, local_expected$path)))
+		
+		local_altered <- with(local_expected, filename[local_md5 != checksum])
+		
+		if(length(local_altered)){
+			warning('Local copies of dataset files have been modified', paste(local_unexpected, collapse=', '))
+		}
+	}
+	
+	# set the directory in options and cache the index 
+	options(safedata.dir = dir)
+	index <- set_index_availability(index)
+	assign('index', index, safedata.env)
+	
+	return(invisible())
+}
+
+
+validate_record_ids <- function(record_ids){
+	
+	#' Validates dataset record ids from user input
+	#'
+	#' This takes a vector of user supplied record identifiers and validates them 
+	#' against the index. It looks up the concept id for individual version 
+	#' record ids. Typically the identifiers are integers, but the function
+	#' will also handle Zenodo URLs and DOIs. 
+	#' 
+	#' The function is largely intended for internal use but can be used to 
+	#' validate records before giving them to a function - the function returns
+	#' an object with class \code{safe_record_set} that allows other functions
+	#' to skip revalidation.
+	#'
+	#' @param record_ids A vector of record_id values
+	#' @return An object of class 'safe_record_set': a dataframe with rownames
+	#'   giving the user provided values and columns \code{concept} and \code{record}
+	#'   indicating the matching records. Note that \code{record} will be NA when the
+	#'   value is a concept id and both fields will be NA if no match is found.
+	#' @examples
+	#'   validate_record_ids(c(3247631, 3266827, 3266821, -1000))
+	#'   validate_record_ids(c('https://doi.org/10.5281/zenodo.3247631', 
+	#'						   '10.5281/zenodo.3266827', 
+	#'						   'https://zenodo.org/record/3266821',
+	#'						   'not_this_one/3266821'))
+	#' @export
+		
+	id_mode <- mode(record_ids)
+	if(! (is.vector(record_ids) && id_mode %in% c('character', 'numeric'))){
+		stop('record_ids must be a character or numeric vector')
+	}
+	
+	# store original versions
+	user <- record_ids
+		
+	if(mode(record_ids) == 'numeric'){
+		
+		# If numbers, look for positive integers
+		not_int <- record_ids %% 1 != 0
+		not_pos <- record_ids <= 0
+		valid <- record_ids[(! not_int) & (! not_pos)]
+
+		if(any(! valid)){
+			warning('Some record ids are not positive integers')
+		}
+				
+	} else if(mode(record_ids) == 'character'){
+		
+		# If string look for one of the possible string representations
+		# of the record: a straight string of the integer or
+		# could be a DOI (possibly with URL) or a Zenodo URL
+		# - https://www.zenodo.org/record/3247631#.XRreJ9NKgWo
+		# - https://doi.org/10.5281/zenodo.3247631
+		# - 10.5281/zenodo.3247631
+
+		match <- regexpr('^[0-9]+$|(?<=record/)[0-9]+|(?<=zenodo.)[0-9]+', record_ids, perl=TRUE)
+		valid <- match != -1
+		
+		if(any(! valid)){
+			warning('Some record ids do not match known id formats')
+		}
+		
+		out <- rep(NA,length(record_ids))
+		out[match != -1] <- regmatches(record_ids, match)
+		record_ids <- as.numeric(out)
+		valid <- match != -1
+	}
+	
+	index <- get_index()
+		
+	# Do they appear in the index		
+	known <- record_ids[valid] %in% c(index$zenodo_record_id, index$zenodo_concept_id)
+	
+	if(! all(known)){
+		warning('Some values are not known concept or record ids')
+	}
+			
+	record_ids <- data.frame(concept=ifelse(record_ids %in% index$zenodo_concept_id, record_ids, NA),
+							 record= ifelse(record_ids %in% index$zenodo_record_id, record_ids, NA))
+	
+	record_ids$concept <- ifelse(is.na(record_ids$concept), 
+								 index$zenodo_concept_id[match(record_ids$record, index$zenodo_record_id)],
+								 record_ids$concept)
+								 
+	attr(record_ids, 'validated') <- TRUE
+	rownames(record_ids) <- user
+	return(record_ids)	
 }
 
 
 get_record_metadata <- function(record_id){
+	
 	#' Get SAFE dataset metadata
 	#'
 	#' Internal handler to load a local copy of the record metadata or retrieve
 	#' it from the SAFE project website if there is no local copy. Much of this
 	#' data is also available from Zenodo, but the SAFE API includes taxon and
 	#' location metadata.
+	#'
 	#' @keywords internal
 	
 	# Check the record id is in the index and get the concept id
+	
 	dir <- get_data_dir()
 	index <- get_index()
 	if(! record_id %in% index$zenodo_record_id){
@@ -168,43 +384,6 @@ get_record_metadata <- function(record_id){
 	}
 	
 	return(record)
-}
-
-update_safe_data_index <- function(){
-	
-	#' Updates the SAFE data index
-	#' 
-	#' This function compares the local index file ('safe_data_index.rds') 
-	#' and attempts to update it from the SAFE Project website API. It 
-	#' requires access to the internet.
-	#' @export
-	
-	dir <- get_data_dir()
-	local <- get_index()
-	
-	# Get the local current index from the website
-	current <- jsonlite::fromJSON('https://www.safeproject.net/api/files')
-	
-	# Currently just using the set of zenodo record ids to test.
-	# It is conceivable that entries could change after being created,
-	# so maybe need a more complex reset at some point to look for 
-	# datasets that have changed.
-	if(setequal(local$zenodo_record_id, current$zenodo_record_id)) {
-		message('Index up to date')
-	} else if(length(setdiff(local$zenodo_record_id, current$zenodo_record_id)) > 0){
-		message('Local index contains retracted or unknown records, contact data@safeproject.net')
-	} else {
-		message('Updating index')
-		# merge in the information on local copies and set local copy 
-		# for new rows to FALSE
-		local <- subset(local, select=c(zenodo_record_id, local_copy))
-		current <- merge(current, local, by='zenodo_record_id')
-		current$local_copy[is.na(current$local_copy)] <- FALSE
-		
-		# Update the index cache and RDS file
-		saveRDS(current, file.path(dir, 'safe_data_index.rds'))
-		assign('index', current, safedata.env)
-	}
 }
 
 concept_summary <- function(record_id){
@@ -332,7 +511,7 @@ record_summary <- function(record_id){
 	cl_nch <- max(ceiling(log10(dwksh$max_col)), 4)
 	rw_nch <- max(ceiling(log10(dwksh$n_data_row)), 4)
 
-	cat('\nData worksheets:')
+	cat('\nData worksheets:\n')
 	cat(sprintf('%*s %*s %*s %s', nm_nch, 'name', cl_nch, 'ncol', 
 				rw_nch, 'nrow', 'description'), sep='\n')
 	
