1609923fea56511dea5291afd75b7593d78d81d2
dorme <d.orme@imperial.ac.uk>
2019-07-29 15:56:10 +0100

    Committing file changes, not just renaming

--- R/metadata.R
+++ R/metadata.R
@@ -1,3 +1,246 @@
+validate_record_ids <- function(record_set){
+	
+	#' Validates dataset record ids from user input
+	#'
+	#' This takes a vector of user supplied record identifiers and validates them 
+	#' against the index. Typically the identifiers are provided as integers, but
+	#' the function will also handle Zenodo URLs and DOIs.
+	#' 
+	#' The function returns a data frame with class \code{safe_record_set}, containing
+	#' the columns \code{concept}, \code{record}, \code{available} and, finally,
+	#' \code{mra} containing the most recent available record (if any). The function can
+	#' be run on an existing \code{safe_record_set} to update this information.
+	#' 
+	#' This function is largely used internally to validate user inputs and to provide
+	#' a common output for the search functions but is exported to allow users to 
+	#' check record ids and display summary information using the print method.
+	#'
+	#' @param record_set A vector of values containing Zenodo concept or record ids.
+	#' @param x An object of class 'safe_record_set'
+	#' @param ... Further arguments to print methods, unused.
+	#' @return An object of class 'safe_record_set': a dataframe with columns
+	#'   \code{concept} and \code{record} indicating the matching records. Note
+	#'   that \code{record} will be NA when the value is a concept id. Inputs
+	#'   that do not match a record or concept ids are returned as an attribute of
+	#'   the record set.
+	#' @examples
+	#'   validate_record_ids(c(3247631, 3266827, 3266821, -1000))
+	#'   validate_record_ids(c('https://doi.org/10.5281/zenodo.3247631', 
+	#'						   '10.5281/zenodo.3266827', 
+	#'						   'https://zenodo.org/record/3266821',
+	#'						   'not_this_one/3266821'))
+	#' @export
+	
+	index <- retrieve_index()
+	
+	# Only run validation if the input isn't already a record set
+	if(! inherits(record_set, 'safe_record_set')){
+	
+		# Otherwise validate
+		if(! (is.vector(record_set) && mode(record_set) %in% c('character', 'numeric'))){
+			stop('record_set must be a character or numeric vector')
+		}
+	
+		# store original versions
+		user <- record_set
+		
+		if(mode(record_set) == 'numeric'){
+		
+			# If numbers, look for positive integers
+			not_int <- record_set %% 1 != 0
+			not_pos <- record_set <= 0
+			valid <- (! not_int) & (! not_pos)
+
+			if(any(! valid)){
+				warning('Some record ids are not positive integers')
+			}
+				
+		} else if(mode(record_set) == 'character'){
+		
+			# If string look for one of the possible string representations
+			# of the record: a straight string of the integer or
+			# could be a DOI (possibly with URL) or a Zenodo URL
+			# - https://www.zenodo.org/record/3247631#.XRreJ9NKgWo
+			# - https://doi.org/10.5281/zenodo.3247631
+			# - 10.5281/zenodo.3247631
+
+			match <- regexpr('^[0-9]+$|(?<=record/)[0-9]+|(?<=zenodo.)[0-9]+', record_set, perl=TRUE)
+			valid <- match != -1
+		
+			if(any(! valid)){
+				warning('Some record ids do not match known id formats')
+			}
+		
+			out <- rep(NA,length(record_set))
+			out[match != -1] <- regmatches(record_set, match)
+			record_set <- as.numeric(out)
+			valid <- match != -1
+		}
+			
+		# Do they appear in the index as concept ids or record ids
+		known <- record_set %in% c(index$zenodo_record_id, index$zenodo_concept_id)
+	
+		if(! all(known)){
+			warning('Some values are not known concept or record ids')
+		}
+			
+		record_set <- data.frame(concept = ifelse(record_set %in% index$zenodo_concept_id, record_set, NA),
+								 record = ifelse(record_set %in% index$zenodo_record_id, record_set, NA))
+	
+		record_set$concept <- ifelse(is.na(record_set$concept), 
+									 index$zenodo_concept_id[match(record_set$record, index$zenodo_record_id)],
+									 record_set$concept)
+		
+	 	class(record_set) <- c('safe_record_set', 'data.frame')
+
+		# remove duplicates
+		record_set <- unique(record_set)
+		
+		# move bad records to attributes
+		record_set <- subset(record_set, valid & known)
+		mismatches <- unique(user[! known | ! valid])
+		if(length(mismatches)){
+			attr(record_set, 'mismatches') <- mismatches
+		}
+	}
+	
+	# Now add information on whether individual records are available and then, for the concept,
+	# the most recent record (which might not be available) and the most recent available if there 
+	# is one
+    record_set$available <- index$available[match(record_set$record, index$zenodo_record_id)]
+
+	most_recent <- subset(index, most_recent, select=c(zenodo_concept_id, zenodo_record_id))
+	record_set$most_recent <- most_recent$zenodo_record_id[match(record_set$concept, most_recent$zenodo_concept_id)]
+	
+	mra <- subset(index, most_recent_available, select=c(zenodo_concept_id, zenodo_record_id))
+	record_set$mra <- mra$zenodo_record_id[match(record_set$concept, mra$zenodo_concept_id)]
+
+	# Sort by concept id (increasing from earliest) and then by record id (decreasing from 
+	# most recent) and keep NAs at the top, so concept ids come first.
+	record_set <- record_set[order(record_set$concept, record_set$record, 
+									decreasing=c(FALSE, TRUE), method='radix', na.last=FALSE), ]
+	
+	rownames(record_set) <- seq_along(record_set$record)
+	return(record_set)	
+}
+
+
+print.safe_record_set <- function(x, ...){
+	
+	#' @describeIn validate_record_ids Print a brief summary of 'safe_record_set' objects.
+	#' @export
+	
+	msg <- paste0('Set includes %i concept ids and %i record ids: \n',
+				  ' - %i open and most recent (*)\n',
+				  ' - %i open and outdated (o)\n',
+				  ' - %i under embargo or restricted (x)\n\n')
+	
+	# record availability flags and counts 
+	x$available <- with(x, ifelse(is.na(record), '', ifelse(! available, 'x', ifelse(record == mra, '*','o'))))
+	n_status <- c('*'=0, 'x'=0, 'o'=0)
+	counts <- table(x$available)
+	n_status[names(counts)] <- counts
+	n_records <- sum(!is.na(x$record))
+		
+	cat(sprintf(msg, length(unique(x$concept)), n_records, 
+					 n_status['*'], n_status['o'], n_status['x']))
+	
+	# This relies on the sort order set in validate_record_ids
+	x$concept <- ifelse(duplicated(x$concept), '-------', x$concept)
+	x$record <- ifelse(is.na(x$record), '-------', x$record)
+	
+	class(x) <- 'data.frame'
+	print(subset(x, select=c(concept, record, available)))
+
+	
+	if(! is.null(attr(x, 'mismatches'))){
+		cat('\nUnmatched record ids:', paste(attr(x, 'mismatches'), sep=', '), '\n')
+	}
+	
+	return(invisible())
+}
+
+
+fetch_record_metadata <- function(record_set){
+	
+	#' Get SAFE dataset metadata
+	#'
+	#' Internal handler to ensure there are local copies of record metadata,
+	#' fetching it from the SAFE project website if needed. This is the same
+	#' data used to populate the Zenodo description but is machine readable
+	#' and contains additional taxon and location indexing.
+	#'
+	#' @param record_set An object of class \code{safe_record_set}.
+	#' @return NULL
+	#' @examples
+	#'   recs <- c('https://doi.org/10.5281/zenodo.3247631', '10.5281/zenodo.3266827', 
+	#'	           'https://zenodo.org/record/3266821', 'not_this_one/3266821')
+	#'   recs <- validate_record_ids(recs)
+	#'   fetch_record_metadata(recs)
+	#' @keywords internal
+	
+	# Check the input class
+	if(! inherits(record_set, 'safe_record_set')){
+		stop('Expects a safe_record_set object.')
+	}
+	
+	# Look for zenodo_record_id files only
+	record_set <- unique(subset(record_set, ! is.na(record)))
+
+	if(nrow(record_set)){
+		
+		safedir <- get_data_dir()
+	
+		# Find missing RDS files
+		record_set$local_path <- with(record_set, file.path(safedir, concept, record, sprintf('%i.json', record)))
+		record_set$to_download <- ! file.exists(record_set$local_path)
+		
+		record_set <- subset(record_set, to_download)
+		
+		if(nrow(record_set)){
+			verbose_message('Downloading ', nrow(record_set), ' record metadata files\n')
+		}
+		
+		for(idx in seq_along(record_set$record)){
+			to_get <- record_set[idx,]
+			
+			if(! dir.exists(dirname(to_get$local_path))){
+				dir.create(dirname(to_get$local_path), recursive=TRUE)
+			}
+			
+			api <- sprintf('https://www.safeproject.net/api/record/%i', to_get$record)
+			curl::curl_download(api, to_get$local_path)
+		}
+	}
+	
+	return(invisible())
+}
+
+
+load_record_metadata <- function(record_set){
+	
+	#' Loads the metadata for a record
+	#'
+	#' @param record_set An object of class \code{safe_record_set} containing a single
+	#'   row with complete concept and record data.
+	#' @keywords internal
+
+	if((! inherits(record_set, 'safe_record_set')) && 
+	   (nrow(record_set) == 1) && 
+	   (! any(is.na(record_set)))
+	  ){
+		stop('Expects a single row safe_record_set object with complete concept and record id.')
+	}
+
+	# Ensure it is locally available 
+	fetch_record_metadata(record_set)
+	
+	# load it and return it
+	safedir <- get_data_dir()
+	metadata_file <- with(record_set, file.path(safedir, concept, record, sprintf('%i.json', record)))
+	return(jsonlite::fromJSON(metadata_file))
+}
+
 
 show_concepts <- function(obj){
 	
@@ -21,8 +264,8 @@ show_concepts <- function(obj){
 	#'    \item An already validated \code{safe_record_set} object, or
 	#'    \item A \code{safedata} data frame loaded using \code{load_safe_data}.
 	#' }
-	#' is not itself a concept id, then the function looks up the relevant 
-	#' concept. The version table indicates which versions are available ('*' 
+	#' If \code{show_concepts} is passed a record id, then the function looks up the
+	#' relevant concept. The version table indicates which versions are available ('*' 
 	#' for the most recent available version and 'o' for older available versions),
 	#' and which are unavailable due to embargo or retriction ('x').
 	#'
