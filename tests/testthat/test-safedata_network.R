0d1b0d06ee4fd076f9da827f8493edbc214578bd
dorme <d.orme@imperial.ac.uk>
2022-04-25 12:39:17 +0100

    Updating files for release, another fix to tests

--- tests/testthat/test-safedata_network.R
+++ tests/testthat/test-safedata_network.R
@@ -22,6 +22,10 @@ internet_unavailable <- function() {
 
 test_that("bad host fails gracefully", {
 
+    if (internet_unavailable()) {
+        skip("No internet - skipping test")
+    }
+
     success <- safedata:::try_to_download("https://httpbinzzzzz.org")
 
     expect_false(success)
@@ -31,7 +35,11 @@ test_that("bad host fails gracefully", {
 
 test_that("timeout fails gracefully", {
 
-    success <- safedata:::try_to_download("https://httpbin.org/delay/2",
+    if (internet_unavailable()) {
+        skip("No internet - skipping test")
+    }
+
+     success <- safedata:::try_to_download("https://httpbin.org/delay/2",
                                           timeout = 1)
     expect_false(success)
     expect_match(attr(success, "fail_msg"), regexp = "URL timed out")
@@ -40,6 +48,10 @@ test_that("timeout fails gracefully", {
 
 test_that("URL errors fails gracefully", {
 
+    if (internet_unavailable()) {
+        skip("No internet - skipping test")
+    }
+
     success <- safedata:::try_to_download("https://httpbin.org/status/404")
     expect_false(success)
     expect_match(attr(success, "fail_msg"), regexp = "URL error")
@@ -47,6 +59,10 @@ test_that("URL errors fails gracefully", {
 })
 
 test_that("Good URL works and returns object to memory", {
+
+    if (internet_unavailable()) {
+        skip("No internet - skipping test")
+    }
 
     success <-  safedata:::try_to_download("https://httpbin.org/base64/c2FmZWRhdGE=")
 
