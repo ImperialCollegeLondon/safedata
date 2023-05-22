a56fdc0333e8a985108894d1771dd439d3d2ebf5
David Orme <davidorme@users.noreply.github.com>
2023-05-22 11:05:46 +0100

    Removing superfluous script for linting

--- build_scripts/collect_lint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This script runs linting across the code and saves the output in the 
-# package root. Note that this uses the .lintr file also in package root
-# to configure linting and manage exceptions.
-
-cd ../
-Rscript -e "lintr::lint_package()" > lint.txt
